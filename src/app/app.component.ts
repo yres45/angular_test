7810848f7a88f95c73f4595c89d6ae9593cf5c7c
Yres <yres22@gmail.com>
2019-09-05 14:51:11 +0300

    create class and  displaying data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+     <h1>{{title}}</h1>
+     <h2> My favorite hero is: {{myHero.name}}</h2>     
+     <p>Heroes:</p>
+     <ul>
+        <li *ngFor="let hero of heroes">
+            {{hero.name}}        
+        </li>
+     </ul>
+     <p *ngIf="heroes.length > 3">There are many heroes!</p>
+     `
+    
 })
 export class AppComponent {
-  title = 'angular2';
+  title = 'Tour of Heroes';
+  heroes = [
+    new Hero(1, 'Yres'),  
+    new Hero(13, 'Alex'),
+    new Hero(15, 'Andrey'),
+    new Hero(4, 'Toma')
+  ];
+  myHero = this.heroes[0];
+
+  
+  
 }
